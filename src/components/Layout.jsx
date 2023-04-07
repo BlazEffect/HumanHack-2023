f860e85070ec3be4e999b15584b94b332bbffe48
makasinui <mks.shi@ya.ru>
2023-04-07 22:22:30 +0300

    drawing, navbar improve

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,9 +3,9 @@ import Navbar from "./Navbar";
 export default function Layout({ children }) {
 
     return (
-        <section className="md:mx-4 h-full flex">
+        <section className="md:mx-4 h-full flex flex-col">
             <Navbar />
-            {children}
+            <div>{children}</div>
         </section>
     )
 }
--- src/pages/Drawing.jsx
+++ src/pages/Drawing.jsx
@@ -1,13 +1,16 @@
 import DrawingArea from "../components/DrawingArea";
+import Layout from "../components/Layout";
 
 export default function Drawing() {
   return (
     <>
-      <h1>Drawing</h1>
-      <div className="border-">
-            <DrawingArea />
-      </div>
-      <button>Save</button>
+      <Layout>
+         
+          <div className="border-">
+                <DrawingArea />
+          </div>
+         
+      </Layout>
     </>
   );
 }
