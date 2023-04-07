3801436cac8e9dbda06838b2755a8b220740cf55
BlazEffeck <kamalinigor@gmail.com>
2023-04-07 23:03:56 +0300

    Create alphabet lesson page

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Drawing from '../pages/Drawing';
 import Main from '../pages/Main';
+import Alphabet from '../pages/Alphabet.jsx';
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: (<Main />)
+    },
+    {
+        path: '/alphabet',
+        element: (<Alphabet />)
     },
     {
         path: '/draw',
